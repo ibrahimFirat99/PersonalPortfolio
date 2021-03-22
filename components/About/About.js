6cf62fd1bf25742bc3c8e40a663a483b11abde8a
ibrahimFirat99 <ibo.f@hotmail.com>
2021-03-22 21:30:57 +0200

    About section completed

--- components/About/About.js
+++ components/About/About.js
@@ -2,18 +2,23 @@ import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <div className={styles.wrapper}>
+    <div id="About" className={styles.wrapper}>
       <h1>
         Hey, I am <span>Ibrahim Firat!</span>
       </h1>
       <p>
-        I am a Full-Stack Web Developer and Data Scientist. I have experience
-        with a range of different programming languages (JavaScript, Python,
-        Java, C, C++, C#), frameworks (ReactJS, NextJS) and technologies
-        (Databases, AWS). Every project needs a different skillset depending on
-        the given requirements. I can covert your dreams and ideas into reality.{" "}
-        <a href="#Contact">Contact me!</a>
+        I am a Full-Stack Web Developer and Data Scientist. I have worked with
+        different programming languages (JavaScript, Python, Java, C, C++, C#),
+        frameworks (ReactJS, NextJS) and technologies (Databases, AWS). Every
+        project needs a different skillset depending on the given requirements.
+        I can help you with coverting your dreams and ideas into reality.{" "}
+        <a id={styles.contact} href="#Contact">
+          Contact me!
+        </a>
       </p>
+      <a id={styles.resume} href="CV.pdf" target="_blank">
+        My Resume
+      </a>
     </div>
   );
 };
