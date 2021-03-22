623113eb988a18ba3b0b990ed8ec6fc759fc268f
ibrahimFirat99 <ibo.f@hotmail.com>
2021-03-22 18:01:37 +0200

    About section content writing

--- components/About/About.js
+++ components/About/About.js
@@ -1,7 +1,21 @@
 import styles from "./About.module.css";
 
 const About = () => {
-  return <h1>Hey I am Ibrahim!</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <h1>
+        Hey, I am <span>Ibrahim Firat!</span>
+      </h1>
+      <p>
+        I am a Full-Stack Web Developer and Data Scientist. I have experience
+        with a range of different programming languages (JavaScript, Python,
+        Java, C, C++, C#), frameworks (ReactJS, NextJS) and technologies
+        (Databases, AWS). Every project needs a different skillset depending on
+        the given requirements. I can covert your dreams and ideas into reality.{" "}
+        <a href="#Contact">Contact me!</a>
+      </p>
+    </div>
+  );
 };
 
 export default About;
