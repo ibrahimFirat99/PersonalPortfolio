f6354bf35df5af27d19f96427d6a3c949d18281a
ibrahimFirat99 <ibo.f@hotmail.com>
2021-03-23 23:12:17 +0200

    Milestones

--- components/About/About.js
+++ components/About/About.js
@@ -3,22 +3,57 @@ import styles from "./About.module.css";
 const About = () => {
   return (
     <div id="About" className={styles.wrapper}>
-      <h1>
-        Hey, I am <span>Ibrahim Firat!</span>
-      </h1>
-      <p>
-        I am a Full-Stack Web Developer and Data Scientist. I have worked with
-        different programming languages (JavaScript, Python, Java, C, C++, C#),
-        frameworks (ReactJS, NextJS) and technologies (Databases, AWS). Every
-        project needs a different skillset depending on the given requirements.
-        I can help you with coverting your dreams and ideas into reality.{" "}
-        <a id={styles.contact} href="#Contact">
-          Contact me!
+      <div className={styles.about}>
+        <h1>
+          Hey, I am <span>Ibrahim Firat!</span>
+        </h1>
+        <p>
+          I am a Full-Stack Web Developer and Data Scientist. I have worked with
+          different programming languages (JavaScript, Python, Java, C, C++,
+          C#), frameworks (ReactJS, NextJS) and technologies (Databases, AWS).
+          Every project needs a different skillset depending on the given
+          requirements. I can help you with coverting your dreams and ideas into
+          reality.{" "}
+          <a className={styles.contact} href="#Contact">
+            Contact me!
+          </a>
+        </p>
+        <a className={styles.resume} href="IbrahimFiratCV.pdf" target="_blank">
+          My Resume
         </a>
-      </p>
-      <a id={styles.resume} href="CV.pdf" target="_blank">
-        My Resume
-      </a>
+      </div>
+      <div className={styles.milestones}>
+        <h2>Milestones</h2>
+        <div className={styles.eventWrapper}>
+          <h3>2021</h3>
+          <div className={styles.event}>
+            <h4>Started pursuing Master's degree</h4>
+            <p>
+              I started pursuing a Master's Degree in Computer Engineering at
+              Eastern Mediterranean University.
+            </p>
+          </div>
+        </div>
+        <div className={styles.eventWrapper}>
+          <h3>2020</h3>
+          <div className={styles.event}>
+            <h4>
+              Designed & Developed{" "}
+              <a href="https://maribotech.com" target="_blank">
+                Maribotech!
+              </a>
+            </h4>
+            <p>I designed and developed a blog website with a friend.</p>
+          </div>
+          <div className={styles.event}>
+            <h4>Graduated University</h4>
+            <p>
+              I graduated from the University of Reading with First Class Honors
+              in Computer Science.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
